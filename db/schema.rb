b2372278c1015cc4cd9a80a859a5df3b0578f1c4
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2023-11-15 19:22:19 +0100

    Add UUID

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 20_231_115_181_334) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension 'pgcrypto'
   enable_extension 'plpgsql'
 end
