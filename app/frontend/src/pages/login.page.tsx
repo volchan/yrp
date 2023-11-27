449cd689aca238020f54a81354ec024e68b960de
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2023-11-27 23:54:49 +0100

    Add redirect to homepage from loggin if logged in

--- app/frontend/src/pages/login.page.tsx
+++ app/frontend/src/pages/login.page.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import Container from '../components/Container'
 import LoginForm from '../components/LoginForm'
+import { useAuth } from '../provider/auth.provider'
 
 const LoginPage = () => {
+  const { accessToken } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (accessToken) {
+      return navigate('/', { replace: true })
+    }
+  }, [])
+
   return (
     <Container>
       <h1>Login</h1>
